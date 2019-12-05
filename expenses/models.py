89e78b0837e4295c9ad2c0f642e3230b9468532c
Valentine <raisen95@gmail.com>
2019-12-05 23:19:43 +0300

    Add data table on workplace

--- expenses/models.py
+++ expenses/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Record(models.Model):
+    transaction = models.FloatField(max_length=30)
+    category = models.CharField(max_length=30)
+    place = models.CharField(max_length=30)
+    date = models.DateTimeField(auto_now_add=True)
--- expenses/views.py
+++ expenses/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from expenses.models import Record
+from django.views.generic import ListView
 
 # Create your views here.
+def all_records(request):
+    a = 10
+    records = Record.objects.all()
+    return render(request, 'workplace.html', {'records': records})
+
+class RecordListView(ListView):
+    queryset = Record.objects.all()
+    context_object_name = 'records'
+    template_name = 'workplace.html'
