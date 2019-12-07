77d21bb00b015fe9b609ce38c2abffc0b9b7ac47
MrRaisRay <MrRaisRay@github.com>
2019-12-07 18:59:45 +0300

    Fix log-in page

--- expenses/views.py
+++ expenses/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from expenses.models import Record
+from expenses.models import Record, Profile
 from expenses.forms import SendEmailForm, LoginForm
 from django.core.mail import send_mail
 from django.views.generic import ListView
