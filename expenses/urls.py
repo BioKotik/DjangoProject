40c6b8daaa1340a3223158f1bf6b2254d84b4d46
MrRaisRay <MrRaisRay@github.com>
2019-12-07 20:45:59 +0300

    Add url

--- expenses/urls.py
+++ expenses/urls.py
@@ -7,6 +7,7 @@ app_name = 'expenses'
 urlpatterns = [
     #path('', views.all_records, name='login'),
     path('', views.user_login, name='list_view'),
-    path('login/', views.authenticate, name='login'),
+    path('workplace/', views.all_records, name='workplace')
+    path('login/', views.user_login, name='login'),
     path('logout/', au_views.LogoutView.as_view(), name='logout'),
 ]
