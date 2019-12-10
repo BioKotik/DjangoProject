5b676dc4f5221a70f022718ce43489db957d0405
MrRaisRay <MrRaisRay@github.com>
2019-12-10 19:55:59 +0300

    Fix migrations and models

--- expenses/urls.py
+++ expenses/urls.py
@@ -7,7 +7,7 @@ app_name = 'expenses'
 urlpatterns = [
     #path('', views.all_records, name='login'),
     path('', views.user_login, name='list_view'),
-    path('workplace/', views.all_records, name='workplace')
+    path('workplace/', views.all_records, name='workplace'),
     path('login/', views.user_login, name='login'),
     path('logout/', au_views.LogoutView.as_view(), name='logout'),
 ]
